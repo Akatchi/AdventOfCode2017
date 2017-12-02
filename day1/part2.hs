da8881f4c7b44d823ea9fc0eb8dfe2d30b145658
Vasco <vascodekrijger@gmail.com>
2017-12-02 18:12:09 +0100

    Minor variable renames

--- day1/part2.hs
+++ day1/part2.hs
@@ -4,17 +4,14 @@ import Data.Char
 nextItemInList :: Int -> [Int] -> Int
 nextItemInList index list = list !! ((index + 1) `mod` (length list))
 
-getHalveTheSize :: [Int] -> Int
-getHalveTheSize list = (length list) `div` 2
+getHalfwayAroundItem :: Int -> [Int] -> Int
+getHalfwayAroundItem index list = nextItemInList ((index - 1) + length list `div` 2) list
 
-nextItemInListPartB :: Int -> [Int] -> Int
-nextItemInListPartB index list = nextItemInList ((index - 1) + getHalveTheSize list) list
+solveCaptcha :: [Int] -> Int
+solveCaptcha list = sum [calculateCaptchaInput (getHalfwayAroundItem a list) b | (a, b) <- zip [0..] list]
 
-getListWithIndex :: [Int] -> Int
-getListWithIndex list = sum [doMagic (nextItemInListPartB a list) b | (a, b) <- zip [0..] list]
-
-doMagic :: Int -> Int -> Int
-doMagic a b = if a == b then a else 0
+calculateCaptchaInput :: Int -> Int -> Int
+calculateCaptchaInput a b = if a == b then a else 0
 
 toIntArray :: String -> [Int]
 toIntArray string = map digitToInt string
@@ -23,11 +20,11 @@ readFromFile :: FilePath -> IO [Int]
 readFromFile = fmap toIntArray . readFile
 
 main = do
-  print("TestA should be 6: " ++ (show $ getListWithIndex [1, 2, 1, 2] == 6))
-  print("TestB should be 0: " ++ (show $ getListWithIndex [1, 2, 2, 1] == 0))
-  print("TestC should be 4: " ++ (show $ getListWithIndex [1, 2, 3, 4, 2, 5] == 4))
-  print("TestD should be 12: " ++ (show $ getListWithIndex [1, 2, 3, 1, 2, 3] == 12))
-  print("TestE should be 4: " ++ (show $ getListWithIndex [1, 2, 1, 3, 1, 4, 1, 5] == 4))
+  print("TestA should be 6: " ++ (show $ solveCaptcha [1, 2, 1, 2] == 6))
+  print("TestB should be 0: " ++ (show $ solveCaptcha [1, 2, 2, 1] == 0))
+  print("TestC should be 4: " ++ (show $ solveCaptcha [1, 2, 3, 4, 2, 5] == 4))
+  print("TestD should be 12: " ++ (show $ solveCaptcha [1, 2, 3, 1, 2, 3] == 12))
+  print("TestE should be 4: " ++ (show $ solveCaptcha [1, 2, 1, 3, 1, 4, 1, 5] == 4))
 
   input <- readFromFile "input"
-  print ("Answer: " ++ (show $ getListWithIndex input))
+  print ("Answer: " ++ (show $ solveCaptcha input))
